7012bac460d09c3977e20a4d00646668a70b9e55
Nicholas Young <nicholas@nicholaswyoung.com>
2014-12-17 11:42:44 -0600

    Initial commit.

---
+++ Readme.md
@@ -0,0 +1,39 @@
+# Telecast for AWS
+
+An AWS provider for the [Telecast](http://github.com/originalmachine/telecast) file management system.
+
+## Installation
+
+```
+npm install telecast-aws --save
+```
+
+## Usage
+
+```javascript
+var telecast = require('telecast');
+
+telecast.configure({
+  aws: {
+    key: 'your amazon access key',
+    secret: 'your amazon secret',
+    container: 'your amazon container'
+  }
+});
+```
+
+Then, follow the [Telecast](http://github.com/originalmachine/telecast) API for uploading, downloading, and removing files.
+
+## License
+
+Copyright (C) Original Machine LLC.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ test/telecast.aws.test.js
@@ -0,0 +1,117 @@
+var AWSProvider = require('../')
+,   fs          = require('fs')
+,   path        = require('path')
+,   expect      = require('chai').expect
+,   Readable    = require('stream').Readable;
+
+describe('AWSProvider: Configuration', function () {
+  it('should require container, key, and secret', function () {
+    try {
+      new AWSProvider();
+    } catch (err) {
+      expect(err).to.not.equal(null);
+    }
+
+    try {
+      new AWSProvider({ container: 'telecast-test' });
+    } catch (err) {
+      expect(err).to.not.equal(null);
+    }
+
+    try {
+      new AWSProvider({
+        container: 'telecast-test',
+        key: 'xxx'
+      });
+    } catch (err) {
+      expect(err).to.not.equal(null);
+    }
+
+    try {
+      new AWSProvider({
+        container: 'telecast-test',
+        key: 'xxx',
+        secret: 'xxx'
+      });
+    } catch (err) {
+      expect(err).to.equal(null);
+    }
+  });
+
+  it('should default to US East 1', function () {
+    expect(new AWSProvider({
+      container: 'telecast-test',
+      key: 'xxx',
+      secret: 'xxx'
+    }).region).to.equal('us-east-1');
+  });
+});
+
+describe('AWSProvider: File Upload', function () {
+  before(function () {
+    this.provider = new AWSProvider({
+      container: 'telecast-test',
+      key: process.env.AWS_KEY,
+      secret: process.env.AWS_SECRET
+    });
+  });
+
+  describe('#put', function () {
+    it('should return a writable stream', function (done) {
+      var input  = path.join(__dirname, 'support/data.txt')
+      ,   output = path.join(__dirname, 'tmp/hello.txt')
+      ,   upload;
+
+      upload = this.provider.put('hello.txt');
+
+      upload.on('success', function (stored) {
+        expect(stored).to.not.equal(null);
+        done();
+      });
+
+      fs.createReadStream(input).pipe(upload);
+    });
+  });
+
+  describe('#get', function () {
+    it('should return a readable stream', function () {
+      expect(this.provider.get('hello.txt') instanceof Readable).to.equal(true);
+    });
+
+    it('should be pipable', function (done) {
+      var output   = path.join(__dirname, 'tmp/test.txt')
+      ,   download = fs.createWriteStream(output)
+      ,   transfer = this.provider.get('hello.txt');
+
+      download.on('error', function (err) {
+        return done(err);
+      });
+
+      download.on('finish', function () {
+        done();
+      });
+
+      transfer.on('error', function (err) {
+        return done(err);
+      });
+
+      transfer.pipe(download);
+    });
+  });
+
+  describe('#del', function () {
+    it('should not return an error when a file is deleted', function (done) {
+      this.provider.del('hello.txt', function (err) {
+        expect(err).to.equal(null);
+        done();
+      });
+    });
+
+    it('should not return an error period', function (done) {
+      this.provider.del('howdy.txt', function (err) {
+        expect(err).to.equal(null);
+        done();
+      });
+    });
+  });
+});
